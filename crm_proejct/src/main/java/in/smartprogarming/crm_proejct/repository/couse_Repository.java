a429bc03f274447cea5a59f72ce1a1d647c0f121
DipanshuTheCoder123 <dipanshudhawade60@gmail.com>
2025-09-01 14:35:34 +0530

    Keep only railway folder

--- crm_proejct/src/main/java/in/smartprogarming/crm_proejct/repository/couse_Repository.java
+++
@@ -1,9 +0,0 @@
-package in.smartprogarming.crm_proejct.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.smartprogarming.crm_proejct.User.Course;
-
-public interface  couse_Repository  extends  JpaRepository<Course, Long>{
-    
-}
